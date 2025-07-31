dda48d596381b1e3e3d62f799521b395af83518a
Rafaelaltieris <raphaelaltieris@gmail.com>
2025-07-31 10:37:39 -0300

    Adicionar arquivos de projeto.

---
+++ AppLoginThiago/App.xaml.cs
@@ -0,0 +1,21 @@
+﻿namespace AppLoginThiago
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+        }
+
+        //
+        protected override Window CreateWindow(IActivationState? activationState)
+        {
+
+            var window = base.CreateWindow(activationState);
+            window.Width = 400;
+            window.Height = 700;
+
+            return new Window(new AppShell());
+        }
+    } //Fecha Classe
+} //Fecha Namespace
---
+++ AppLoginThiago/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace AppLoginThiago
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
